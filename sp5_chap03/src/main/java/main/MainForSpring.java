31ce09c883ca2b436482c5a63ea837eb0ac02c36
Giyong8504 <kky5163@naver.com>
2023-10-16 15:51:39 +0900

    소스 업로드

--- sp5_chap03/src/main/java/main/MainForSpring.java
+++ sp5_chap03/src/main/java/main/MainForSpring.java
@@ -36,6 +36,9 @@ public class MainForSpring {
             } else if (command.startsWith("info")) {
                 processInfoCommand(command.split(" "));
                 continue;
+            } else if (command.equals("version")) {
+                processVersionCommand();
+                continue;
             }
             printHelp();
         }
@@ -103,5 +106,9 @@ public class MainForSpring {
 
         MemberInfoPrinter infoPrinter = ctx.getBean("infoPrinter", MemberInfoPrinter.class);
         infoPrinter.printMemberInfo(arg[1]);
+    }
+    private static void processVersionCommand() {
+        VersionPrinter versionPrinter = ctx.getBean("versionPrinter", VersionPrinter.class);
+        versionPrinter.print();
     }
 }
