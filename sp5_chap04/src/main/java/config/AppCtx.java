85ee4ecee34e6c0e38f9e9916a1aa1606be6d012
Giyong8504 <kky5163@naver.com>
2023-10-17 12:18:41 +0900

    챕터 4 기본 소스 업로드

---
+++ sp5_chap04/src/main/java/config/AppCtx.java
@@ -0,0 +1,54 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.*;
+
+@Configuration
+public class AppCtx {
+
+    @Bean
+    public MemberDao memberDao() {
+        return new MemberDao();
+    }
+
+    @Bean
+    public MemberRegisterService memberRegSvc() {
+        return new MemberRegisterService(memberDao());
+    }
+
+    @Bean
+    public ChangePasswordService changePwdSvc() {
+        ChangePasswordService pwdSvc = new ChangePasswordService();
+        return pwdSvc;
+    }
+
+    @Bean
+    public MemberPrinter memberPrinter() {
+        return new MemberPrinter();
+    }
+
+    @Bean
+    public MemberListPrinter listPrinter() {
+        return new MemberListPrinter(memberDao(), memberPrinter());
+    }
+    @Bean
+    public MemberInfoPrinter infoPrinter() {
+        MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
+        /**
+         * 세터 메서드를 사용해서 의존 주입을 하지 않아도
+         * 스프링 컨테이너가 @Autowired를 붙인 필드에
+         * 자동으로 해당 타입의 빈 객체를 주입
+         */
+        // infoPrinter.setMemberDao(memberDao());
+        // infoPrinter.setPrinter(memberPrinter());
+        return infoPrinter;
+    }
+    @Bean
+    public VersionPrinter versionPrinter() {
+        VersionPrinter versionPrinter = new VersionPrinter();
+        versionPrinter.setMajorVersion(5);
+        versionPrinter.setMinorVersion(0);
+        return versionPrinter;
+    }
+}
