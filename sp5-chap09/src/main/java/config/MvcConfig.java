ee3ebcdeaa097f887f962ca2eb64ff504ba94ffa
Giyong8504 <kky5163@naver.com>
2023-11-01 00:44:26 +0900

    chap09 초기소스 업로드

---
+++ sp5-chap09/src/main/java/config/MvcConfig.java
@@ -0,0 +1,26 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/view/", ".jsp");
+    }
+}
