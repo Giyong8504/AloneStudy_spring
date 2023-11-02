73b2044259eb09cc308e41103ee2fd9665469039
Giyong8504 <kky5163@naver.com>
2023-11-02 11:44:37 +0900

    chap11 초기 업로드

---
+++ sp5-chap11/src/main/java/spring/MemberDao.java
@@ -0,0 +1,100 @@
+package spring;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.*;
+import java.util.List;
+
+public class MemberDao {
+    private JdbcTemplate jdbcTemplate;
+
+    public MemberDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public int count() {
+        Integer count = jdbcTemplate.queryForObject(
+                "SELECT COUNT(*) FROM member", Integer.class);
+        return count;
+    }
+
+    public Member selectByEmail(String email) {
+        List<Member> results = jdbcTemplate.query(
+                "SELECT * FROM member WHERE email = ?",
+                new RowMapper<Member>() {
+                    @Override
+                    public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        Member member = new Member(
+                                rs.getString("email"),
+                                rs.getString("password"),
+                                rs.getString("name"),
+                                rs.getTimestamp("regDate").toLocalDateTime());
+                        member.setId(rs.getLong("id"));
+                        return member;
+                    }
+                },
+                email);
+
+        return results.isEmpty() ? null : results.get(0);
+    }
+
+    public void insert(final Member member) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement pstmt = con.prepareStatement(
+                        "INSERT INTE member (EMAIL, PASSWORD, NAME, REGDATE)" +
+                                "values (?, ?, ?, ?)",
+                        new String[]{"ID"});
+                pstmt.setString(1, member.getEmail());
+                pstmt.setString(2, member.getPassword());
+                pstmt.setString(3, member.getName());
+                pstmt.setTimestamp(4, Timestamp.valueOf(member.getRegisterDateTime()));
+                return pstmt;
+            }
+        }, keyHolder);
+        Number keyValue = keyHolder.getKey();
+        member.setId(keyValue.longValue());
+    }
+
+    public void update(Member member) {
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection conn) {
+                // 파라미터로 전달받은 Connection을 이용해서 PreparedStatement 생성
+                PreparedStatement pstmt = conn.prepareStatement(
+                        "INSERT INTO member(email, password, name, regDate) values (?, ?, ?, ?)");
+                // 인덱스 파라미터 값 설정
+                pstmt.setString(1, member.getEmail());
+                pstmt.setString(2, member.getPassword());
+                pstmt.setString(3, member.getName());
+                pstmt.setTimestamp(4, Timestamp.valueOf(member.getRegisterDateTime()));
+                // 생성한 PreparedStatement 객체 리턴
+                return pstmt;
+            }
+        });
+
+        public List<Member> selectAll () {
+            List<Member> results = jdbcTemplate.query("SELECT * FROM member",
+                    new RowMapper<Member>() {
+                        @Override
+                        public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+                            Member member = new Member(
+                                    rs.getString("email"),
+                                    rs.getString("password"),
+                                    rs.getString("name"),
+                                    rs.getTimestamp("regDate").toLocalDateTime());
+                            member.setId(rs.getLong("id"));
+                            return member;
+                        }
+                    });
+            return results;
+        }
+    }
+}
