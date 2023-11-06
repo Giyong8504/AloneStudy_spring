8ff1115cf39df9131924f90e7bc4201e2c7d8109
Giyong8504 <kky5163@naver.com>
2023-11-06 17:12:17 +0900

    session 까지 업로드

--- sp5-chap13/src/main/java/config/ControllerConfig.java
+++ sp5-chap13/src/main/java/config/ControllerConfig.java
@@ -1,11 +1,14 @@
 package config;
 
+import controller.ChangePwdController;
 import controller.LoginController;
+import controller.LogoutController;
 import controller.RegisterController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.AuthService;
+import spring.ChangePasswordService;
 import spring.MemberRegisterService;
 import survey.SurveyController;
 
@@ -13,6 +16,9 @@ import survey.SurveyController;
 public class ControllerConfig {
     @Autowired
     private MemberRegisterService memberRegSvc;
+
+    @Autowired
+    private ChangePasswordService changePasswordService;
 
     @Autowired
     private AuthService authService;
@@ -28,6 +34,18 @@ public class ControllerConfig {
     public LoginController loginController() {
         LoginController controller = new LoginController();
         controller.setAuthService(authService);
+        return controller;
+    }
+
+    @Bean
+    public LogoutController logoutController() {
+        return new LogoutController();
+    }
+
+    @Bean
+    public ChangePwdController changePwdController() {
+        ChangePwdController controller = new ChangePwdController();
+        controller.setChangePasswordService(changePasswordService);
         return controller;
     }
 }
